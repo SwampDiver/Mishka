0fcc88195ae0f15319245f527d0c3bdb4cb1b1b3
SwampDiver <artureichenwald@gmail.com>
2020-12-04 10:01:18 +0700

    Создал файл js

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.getElementById('toggle-button');
+// const navToggle = document.querySelector('main-nav__toggle');
+
+navMain.classList.remove('main-nav--no-js');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
